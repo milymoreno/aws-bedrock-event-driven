79249a1975ee3f1e164ba4befe7691acc9870177
MilyMoreno <milydemendoza@gmail.com>
2025-09-06 19:05:15 -0500

    fix(ci): provider AWS sin profile (OIDC)

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,14 +1,8 @@
-variable "aws_profile" {
-  type    = string
-  default = "mily-aws-student"
-}
-
 variable "aws_region" {
   type    = string
   default = "us-east-1"
 }
 
 provider "aws" {
-  profile = var.aws_profile
-  region  = var.aws_region
+  region = var.aws_region
 }
